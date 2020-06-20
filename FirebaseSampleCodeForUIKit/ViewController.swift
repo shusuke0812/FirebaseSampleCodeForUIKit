105b38fe179dd4b0601a5f07358bc36239499362
shusuke0812 <shu.ota0812@gmail.com>
2020-06-21 00:32:20 +0900

    [add]Home画面のリスト表示の型を実装

--- FirebaseSampleCodeForUIKit/ViewController.swift
+++ FirebaseSampleCodeForUIKit/ViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 5
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用可能なCellを得る
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        // 値を設定する
+        cell.textLabel!.text = "Row \(indexPath.row)"
+        
+        return cell
+        
+       }
 
 
 }
