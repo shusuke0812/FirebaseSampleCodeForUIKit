29b2b3e673073941deffeb0a2487e9e3e65a45d0
shusuke0812 <shu.ota0812@gmail.com>
2020-07-24 10:36:45 +0900

    [fix]セル数をダミー用に増加

--- FirebaseSampleCodeForUIKit/ViewController/ViewController.swift
+++ FirebaseSampleCodeForUIKit/ViewController/ViewController.swift
@@ -27,7 +27,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-           return 5
+           return 15
        }
        
        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
