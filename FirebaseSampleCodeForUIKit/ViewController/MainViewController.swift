8fa62f441a2f689857298b0bc64fc46bf44dbb67
shusuke0812 <shu.ota0812@gmail.com>
2020-08-24 23:22:47 +0900

    [rename]トップページのViewController名を変更

--- FirebaseSampleCodeForUIKit/ViewController/MainViewController.swift
+++ FirebaseSampleCodeForUIKit/ViewController/MainViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var tableView: UITableView!
     
