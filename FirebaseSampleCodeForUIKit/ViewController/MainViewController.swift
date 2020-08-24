69e6e157ca3e809c51cec14aa8951ab2db78f520
shusuke0812 <shu.ota0812@gmail.com>
2020-08-24 23:55:37 +0900

    [add]トップページのセルをタップして画面遷移させるコードを追加

--- FirebaseSampleCodeForUIKit/ViewController/MainViewController.swift
+++ FirebaseSampleCodeForUIKit/ViewController/MainViewController.swift
@@ -12,6 +12,7 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
     
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,18 +20,21 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         tableView.delegate = self
         
         // セルタップを無効にする
+        #if false
         tableView.allowsSelection = false
+        #endif
         
         // TableViewCellを読み込み
         let nib = UINib(nibName: "TableViewCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "Cell")
     }
-    
+        
+    // MARK: - Private Method
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
            return 15
-       }
+    }
        
-       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 再利用可能なCellを得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
@@ -38,9 +42,12 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         cell.textLabel!.text = "Row \(indexPath.row)"
         
         return cell
-        
-       }
-
-
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let s: UIStoryboard = UIStoryboard(name: "MainDetailViewController", bundle: nil)
+        let vc: MainDetailViewController = s.instantiateInitialViewController() as! MainDetailViewController
+        self.present(vc, animated: true, completion: nil)
+    }
 }
 
