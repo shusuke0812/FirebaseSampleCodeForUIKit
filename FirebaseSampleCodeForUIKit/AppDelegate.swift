5f51b4f2b3e515cf85073bc83ab222d03f386d8c
shusuke0812 <shu.ota0812@gmail.com>
2020-08-25 01:40:42 +0900

    [add]Firebaseのプロジェクトを追加

--- FirebaseSampleCodeForUIKit/AppDelegate.swift
+++ FirebaseSampleCodeForUIKit/AppDelegate.swift
@@ -7,18 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Firebase初期化
+        FirebaseApp.configure()
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
+    // MARK: - UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FirebaseSampleCodeForUIKit' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Firebase
+  pod 'Firebase/Analytics'
+
+end
