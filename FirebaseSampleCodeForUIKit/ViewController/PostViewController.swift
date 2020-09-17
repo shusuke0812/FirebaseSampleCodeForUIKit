ca7481ca85a64d366aebe4994cb35a06efe61036
shusuke0812 <shu.ota0812@gmail.com>
2020-09-17 18:24:06 +0900

    [add]Storageへの画像保存処理を追加

--- FirebaseSampleCodeForUIKit/ViewController/PostViewController.swift
+++ FirebaseSampleCodeForUIKit/ViewController/PostViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostViewController: UIViewController {
 
@@ -16,5 +17,17 @@ class PostViewController: UIViewController {
         super.viewDidLoad()
     }
     @IBAction func changeImageViewButton(_ sender: Any) {
+        guard let image = self.imageView?.image else { return }
+        guard let uploadImage = image.jpegData(compressionQuality: 0.3) else { return }
+        
+        let fileName = NSUUID().uuidString
+        let storageRef = Storage.storage().reference().child("user_image").child(fileName)
+        
+        storageRef.putData(uploadImage, metadata: nil) { (metadata, err) in
+            if let err = err {
+                print("DEBUG： FireStorageへの情報保存に失敗しました")
+                print(err)
+            }
+        }
     }
 }
