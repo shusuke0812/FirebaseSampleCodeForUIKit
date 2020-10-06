f0af7ce4ec3da180b8af078fca69b8d0ad9fd12f
shusuke0812 <shu.ota0812@gmail.com>
2020-10-06 09:06:16 +0900

    [add]メッセージ追加関数を追加

--- api/functions/index.js
+++ api/functions/index.js
@@ -2,3 +2,10 @@ const functions = require('firebase-functions');
 
 const admin = require('firebase-admin');
 admin.initializeApp();
+
+// addMessage関数を追加
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+	const original = req.query.text;
+	const writeResult = await admin.firebase().collection('message').add({ original: original });
+	res.json({ result: `Message with ID: ${writeResult.id} added.` });
+});
