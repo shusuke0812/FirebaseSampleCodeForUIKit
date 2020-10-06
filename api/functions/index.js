26e4bd95d2c6842604ccf3a24a7ee07f78059a8a
shusuke0812 <shu.ota0812@gmail.com>
2020-10-06 09:08:43 +0900

    [add]firestoreに書き込みがあった時のトリガー関数を追加

--- api/functions/index.js
+++ api/functions/index.js
@@ -9,3 +9,12 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
 	const writeResult = await admin.firebase().collection('message').add({ original: original });
 	res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
+
+// makeUppercase関数を追加
+exports.makeUppercase = functions.firestore.document('/messages/{documentId}').onCreate( (snap, context) => {
+	const original = snap.data().original;
+	functions.logger.log('Uppercasing', context.params.documentId, original);
+	
+	const uppercase = original.toUpperCase();
+	return snap.ref.set({ uppercase }, { merge: true });
+});
