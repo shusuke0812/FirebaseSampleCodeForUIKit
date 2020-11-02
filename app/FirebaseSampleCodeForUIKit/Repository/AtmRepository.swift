99f19746d40b288052f55ef6aee1eeb72f996f81
shusuke0812 <shu.ota0812@gmail.com>
2020-11-02 21:22:43 +0900

    [add]Firestoreのデータ取得リポジトリを修正

--- app/FirebaseSampleCodeForUIKit/Repository/AtmRepository.swift
+++ app/FirebaseSampleCodeForUIKit/Repository/AtmRepository.swift
@@ -7,19 +7,41 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 protocol AtmRepositoryProtocol {
     /// ATM情報を取得する
+    /// - Parameters:
+    ///   - atmId: atmドキュメントID
+    ///   - onSuccess: 成功した時に呼ばれる
+    ///   - onError: 失敗した時に呼ばれる
     func getAtms(atmId: String,
-                 onSuccess: @escaping () -> Void,
-                 onError: @escaping (String) -> Void)
+                 onSuccess: @escaping (_ atm: Atm) -> Void,
+                 onError: @escaping (Error) -> Void)
 }
 
 class AtmRepository: AtmRepositoryProtocol {
 }
 
 extension AtmRepository {
-    func getAtms(atmId: String, onSuccess: @escaping () -> Void, onError: @escaping (String) -> Void) {
-        print("")
+    func getAtms(atmId: String, onSuccess: @escaping (_ atm: Atm) -> Void, onError: @escaping (Error) -> Void) {
+        let db: Firestore = Firestore.firestore()
+        let ref: DocumentReference = db.collection("atms").document(atmId)
+        
+        ref.getDocument { (snapshot, error) in
+            if let error = error {
+                print("DEBUG: atmの取得に失敗しました -> \(error)")
+            }
+            guard let snapshot = snapshot?.data() else { return }
+            do {
+                let atm = try Firestore.Decoder().decode(Atm.self, from: snapshot)
+                onSuccess(atm)
+            } catch (let error ) {
+                onError(error)
+                return
+            }
+        }
     }
 }
